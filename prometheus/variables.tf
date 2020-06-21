276868815a33be3305af1e6291a5543f4e2e331d
Ronaldo <ronaldolauture@gmail.com>
2020-06-20 21:00:28 -0400

    configuration for prom/graf/alertmanager stack

---
+++ prometheus/variables.tf
@@ -0,0 +1,17 @@
+variable subnetid {
+  type        = string
+  default     = ""
+  description = "ID of your id"
+}
+
+variable securitygroupids {
+  type        = string
+  default     = ""
+  description = "id of your security group"
+}
+
+variable key_name {
+  type        = string
+  default     = ""
+  description = "name of your aws key"
+}
